9507b684acf185710915748b684b9a0d295ec933
johndward01 <jackwade464@gmail.com>
2021-05-24 14:26:52 -0500

    finished

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ console.log(
 );
 
 // Exercise 1
+const firstName = 'John';
+let lastName = 'Ward';
+var age = 30;
 
 // Exercise 2
+let fullName = firstName + ' ' + lastName;
+let altFullName = `${firstName} ${lastName}`;
+console.log(fullName);
+console.log(altFullName);
 
 // Exercise 3
+let city = 'Birmingham';
+let pastTime = 'walking';
+
+let myStory = `Hi, as you might have already guessed I am ${fullName}.
+I'm currently living relatively close to ${city}, 
+and my favorite past time is ${pastTime}.`;
+console.log(myStory);
